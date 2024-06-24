1812292f1f745736686b76fe024d3fc121c6cefa
shiftyLogm <poisondroid456@gmail.com>
2024-06-24 04:45:47 -0300

    feat

---
+++ C#/ArrayPlusArray.cs
@@ -0,0 +1,8 @@
+// https://www.codewars.com/kata/5a2be17aee1aaefe2a000151
+
+using System.Linq;
+
+public static class Kata
+{
+	public static int ArrayPlusArray(int[] arr1, int[] arr2) => arr1.Sum() + arr2.Sum();
+}
